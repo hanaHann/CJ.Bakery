c3d574c804d2a154b89a73582fce117c34261b6c
hanaHann <melody19961110@gmail.com>
2022-04-03 01:17:50 +0800

    導覽列更新

---
+++ cj-code/js/switchFavorite.js
@@ -0,0 +1,32 @@
+
+
+
+
+function switchFavorite(){
+var heart = document.getElementById("heart");
+
+  if(heart.title=="加入收藏"){
+    heart.src="../../texture/heart2.png";
+    heart.title="取消收藏";
+    console.log("成功");
+  }
+  else if (heart.title="取消收藏"){
+    heart.src="../../texture/heart1.png";
+    heart.title="加入收藏";
+    console.log("成功了");
+    
+  }
+
+
+}
+heart.addEventListener("click",switchFavorite);
+// function hoverFavorite(){
+//   heart.src="../../texture/heart2.png";
+//   console.log("hover");
+
+// }
+
+// heart.addEventListener("mouseover",hoverFavorite);
+
+
+
