0aa439b01f2ad4f29e47abd4e9c9e9d33ba685ba
hanaHann <melody19961110@gmail.com>
2022-04-04 00:04:13 +0800

    新增頁面

--- cj-code/js/switchFavorite.js
+++ cj-code/js/switchFavorite.js
@@ -1,25 +1,28 @@
 
 
 
+window.addEventListener("load",switchFavorite){
 
-function switchFavorite(){
-var heart = document.getElementById("heart");
 
-  if(heart.title=="加入收藏"){
-    heart.src="../../texture/heart2.png";
-    heart.title="取消收藏";
-    console.log("成功");
+  function switchFavorite(){
+  var heart = document.getElementById("heart");
+
+    if(heart.title=="加入收藏"){
+      heart.src="../../texture/heart2.png";
+      heart.title="取消收藏";
+      console.log("成功");
+    }
+    else if (heart.title="取消收藏"){
+      heart.src="../../texture/heart1.png";
+      heart.title="加入收藏";
+      console.log("成功了");
+      
+    }
+
+
   }
-  else if (heart.title="取消收藏"){
-    heart.src="../../texture/heart1.png";
-    heart.title="加入收藏";
-    console.log("成功了");
-    
-  }
-
-
+  heart.addEventListener("click",switchFavorite);
 }
-heart.addEventListener("click",switchFavorite);
 // function hoverFavorite(){
 //   heart.src="../../texture/heart2.png";
 //   console.log("hover");
