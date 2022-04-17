e0fa92c04e9a3792587fca146fe5e1e81035f626
hanaHann <melody19961110@gmail.com>
2022-04-18 00:31:04 +0800

    表單rwd

--- js/all.js
+++ js/all.js
@@ -137,7 +137,7 @@ window.addEventListener("load",function(){
             console.log(heart[i])
             if(this.title=="加入收藏"){
                 addHeart.style.visibility="visible"
-                this.src="../texture/heart2.png";        //換圖片
+                this.src="../texture/heart2.png";        //換有愛心的圖片
                 this.title="取消收藏";
                 lightboxBcg.style.display="block";       //背景遮罩
                 lightboxBcg.style.opacity=".3"           //背景遮罩
@@ -146,11 +146,10 @@ window.addEventListener("load",function(){
                     lightboxBcg.style.display="none";
                     
                 },800)
-
             }
             else if (this.title="取消收藏"){
                 removeHeart.style.visibility="visible"
-                this.src="../texture/heart1.png";         //換圖片
+                this.src="../texture/heart1.png";         //換空心的圖片
                 this.title="加入收藏";
                 lightboxBcg.style.display="block";        //背景遮罩
                 lightboxBcg.style.opacity=".3"            //背景遮罩
@@ -158,77 +157,77 @@ window.addEventListener("load",function(){
                     removeHeart.style.visibility="hidden"
                     lightboxBcg.style.display="none";
                 },800)
-                
             }
-            
         })
     }
 })
 
 
 //按鈕切換商品頁面
-
-var dessertBtns = document.getElementsByClassName("btn-2"); //元素們
-var dessertContainers = document.getElementsByClassName("container");
-
-for(let i=0; i<dessertBtns.length; i++){
-    dessertContainers[i].style.display="none";
-    dessertContainers[0].style.display="block";
-    dessertBtns[i].addEventListener("click",function(){
-        this.style.backgroundColor="white";
-        dessertContainers[i].style.display="block";
-            for(let j=0; j<dessertBtns.length; j++){
-                if( i != j){
-                    dessertBtns[j].style.backgroundColor="transparent";
-                    dessertContainers[j].style.display="none";
+window.addEventListener("load",function(){
+    var dessertBtns = document.getElementsByClassName("btn-2"); //元素們
+    var dessertContainers = document.getElementsByClassName("container");
+    
+    for(let i=0; i<dessertBtns.length; i++){
+        dessertContainers[i].style.display="none";
+        dessertContainers[0].style.display="block";
+        dessertBtns[i].addEventListener("click",function(){
+            this.style.backgroundColor="white";
+            dessertContainers[i].style.display="block";
+                for(let j=0; j<dessertBtns.length; j++){
+                    if( i != j){
+                        dessertBtns[j].style.backgroundColor="transparent";
+                        dessertContainers[j].style.display="none";
+                    }
+    
                 }
-
-            }
-    });
-};
+        });
+    };
+})
 
 // 以下是輪播
 
-var slidePic = document.getElementById("slidepic");
-var img = slidePic.querySelector(`.img`);
-var imgAll = slidePic.querySelectorAll(`.img`);
-
-// setInterval(function(){
-//     slidePic.appendChild(img);
-
-// },500)
-
-var num = 0;
-var start = setInterval(function(){
-    num++;
-    for(let i=0 ; i<imgAll.length ;i++){
-    imgAll[i].style.display="none"};
+window.addEventListener("load",function(){
     
-    if( num == imgAll.length ){
-        num = 0;
+    var slidePic = document.getElementById("slidepic");
+    var img = slidePic.querySelector(`.img`);
+    var imgAll = slidePic.querySelectorAll(`.img`);
+    
+    // setInterval(function(){
+    //     slidePic.appendChild(img);
+    
+    // },500)
+    
+    var num = 0;
+    var start = setInterval(function(){
+        num++;
+        for(let i=0 ; i<imgAll.length ;i++){
+        imgAll[i].style.display="none"};
+        
+        if( num == imgAll.length ){
+            num = 0;
+        }
+        imgAll[num].style.display="block";
+    },2000)
+    
+    var slidePic = document.getElementById("slidepic");
+    var btnControlItems = document.querySelectorAll(".btn-control span");
+    console.log(btnControlItems[0])
+    
+    for(let j=0 ; j<btnControlItems.length ; j++){
+        //點擊一下時清除輪播
+        btnControlItems[j].addEventListener("click",function(){clearInterval(start)});
+        //點擊時換按鈕顏色
+        btnControlItems[j].addEventListener("click",function(){
+            this.style.backgroundColor="white";
+        })
+       
     }
-    imgAll[num].style.display="block";
-},2000)
-
-var slidePic = document.getElementById("slidepic");
-var btnControlItems = document.querySelectorAll(".btn-control span");
-console.log(btnControlItems[0])
-
-for(let j=0 ; j<btnControlItems.length ; j++){
-    //點擊一下時清除輪播
-    btnControlItems[j].addEventListener("click",function(){clearInterval(start)});
-    //點擊時換按鈕顏色
-    btnControlItems[j].addEventListener("click",function(){
-        this.style.backgroundColor="white";
-    })
-   
-}
 
 
+})
 
-
-
-// 手機版footer
+// 手機版footer not
 
 window.addEventListener("load",function(){
     let screenWidth = screen.width;
