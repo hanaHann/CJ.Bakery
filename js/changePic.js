81fc4928fdc0c65f7a7d4271f3b1002e8d6d5539
hanaHann <melody19961110@gmail.com>
2022-04-25 12:18:24 +0800

    換圖片 js ok

---
+++ js/changePic.js
@@ -0,0 +1,13 @@
+window.addEventListener("load",function(){
+
+    $(".ds-pic").mouseover(function(){
+        $(".ds-pic2").fadeIn(700);
+        $(this).fadeOut(700);
+    })
+    $(".ds-pic2").mouseout(function(){
+        $(".ds-pic").fadeIn(0);
+        $(".ds-pic2").fadeOut(0);
+        
+        
+    })
+})
--- js/qaClick.js
+++ js/qaClick.js
@@ -5,88 +5,102 @@ window.addEventListener("load",function(){
     console.log(qaInf[0]);
     console.log(qa.length);
 
-    qa[0].addEventListener("click",function(){
-        if( qaInf[0].classList.contains("faq-hidden") == true){
+        qa[0].addEventListener("click",function(){
+            if( qaInf[0].classList.contains("faq-hidden") == true){
 
-        qaInf[0].classList.remove("faq-hidden");
-        }else{
-            qaInf[0].classList.add("faq-hidden")
-        }
-    });
+            qaInf[0].classList.remove("faq-hidden");
+            }else{
+                qaInf[0].classList.add("faq-hidden")
+            }
+        });
 
-    qa[1].addEventListener("click",function(){
-        if( qaInf[1].classList.contains("faq-hidden") == true){
+        qa[1].addEventListener("click",function(){
+            if( qaInf[1].classList.contains("faq-hidden") == true){
 
-        qaInf[1].classList.remove("faq-hidden");
-        }else{
-            qaInf[1].classList.add("faq-hidden")
-        }
-    });
+            qaInf[1].classList.remove("faq-hidden");
+            }else{
+                qaInf[1].classList.add("faq-hidden")
+            }
+        });
 
-    qa[2].addEventListener("click",function(){
-        if( qaInf[2].classList.contains("faq-hidden") == true){
+        qa[2].addEventListener("click",function(){
+            if( qaInf[2].classList.contains("faq-hidden") == true){
 
-        qaInf[2].classList.remove("faq-hidden");
-        }else{
-            qaInf[2].classList.add("faq-hidden")
-        }
-    });
+            qaInf[2].classList.remove("faq-hidden");
+            }else{
+                qaInf[2].classList.add("faq-hidden")
+            }
+        });
 
-    qa[3].addEventListener("click",function(){
-        if( qaInf[3].classList.contains("faq-hidden") == true){
+        qa[3].addEventListener("click",function(){
+            if( qaInf[3].classList.contains("faq-hidden") == true){
 
-        qaInf[3].classList.remove("faq-hidden");
-        }else{
-            qaInf[3].classList.add("faq-hidden")
-        }
-    });
+            qaInf[3].classList.remove("faq-hidden");
+            }else{
+                qaInf[3].classList.add("faq-hidden")
+            }
+        });
 
-    qa[4].addEventListener("click",function(){
-        if( qaInf[4].classList.contains("faq-hidden") == true){
+        qa[4].addEventListener("click",function(){
+            if( qaInf[4].classList.contains("faq-hidden") == true){
 
-        qaInf[4].classList.remove("faq-hidden");
-        }else{
-            qaInf[4].classList.add("faq-hidden")
-        }
-    });
+            qaInf[4].classList.remove("faq-hidden");
+            }else{
+                qaInf[4].classList.add("faq-hidden")
+            }
+        });
 
-    qa[5].addEventListener("click",function(){
-        if( qaInf[5].classList.contains("faq-hidden") == true){
+        qa[5].addEventListener("click",function(){
+            if( qaInf[5].classList.contains("faq-hidden") == true){
 
-        qaInf[5].classList.remove("faq-hidden");
-        }else{
-            qaInf[5].classList.add("faq-hidden")
-        }
-    });
+            qaInf[5].classList.remove("faq-hidden");
+            }else{
+                qaInf[5].classList.add("faq-hidden")
+            }
+        });
 
-    qa[6].addEventListener("click",function(){
-        if( qaInf[6].classList.contains("faq-hidden") == true){
+        qa[6].addEventListener("click",function(){
+            if( qaInf[6].classList.contains("faq-hidden") == true){
 
-        qaInf[6].classList.remove("faq-hidden");
-        }else{
-            qaInf[6].classList.add("faq-hidden")
-        }
-    });
+            qaInf[6].classList.remove("faq-hidden");
+            }else{
+                qaInf[6].classList.add("faq-hidden")
+            }
+        });
 
-    qa[7].addEventListener("click",function(){
-        if( qaInf[7].classList.contains("faq-hidden") == true){
+        qa[7].addEventListener("click",function(){
+            if( qaInf[7].classList.contains("faq-hidden") == true){
 
-        qaInf[7].classList.remove("faq-hidden");
-        }else{
-            qaInf[7].classList.add("faq-hidden")
-        }
-    });
+            qaInf[7].classList.remove("faq-hidden");
+            }else{
+                qaInf[7].classList.add("faq-hidden")
+            }
+        });
 
     // for(i=0 ; i<qa.length ; i++){
     //     qa[i].addEventListener("click",function(){
-    //         if( qaInf[i].classList.contains("faq-hidden") == true){
-    //             alert("hi")
-    //         qaInf[i].classList.remove("faq-hidden");
-    //         }else{
-    //             qaInf[i].classList.add("faq-hidden");
-    //         };
+    //         console.log("點嚕");
+    //         // e.target.style.border="3px solid red";
+    //         for(j=0 ;j<qaInf.length ;j++){
+                
+    //             if( i == j){
+    //                 console.log("一樣嚕");
+    //                 alert("一樣");
+    //                 // if( qaInf[1].classList.contains("faq-hidden") == true){
+    //                 //     // alert("hiiii")
+
+    //                 // qaInf[1].classList.remove("faq-hidden");
+    //                 // }else{
+    //                 //     qaInf[1].classList.add("faq-hidden");
+    //                 // };
+    //             }
+            
+    //         }
+           
     //     });
     // }
+
+
 })
 
 // 熊熊
